d29d13a46501c9bca3a7e829836798de4277071e
Mari-Liis Sink <113007407+mariliis01@users.noreply.github.com>
2023-05-06 10:29:14 +0300

    Delete environment.js

--- environment.js
+++
@@ -1,6 +0,0 @@
-const API_KEY = "19906bfefbmsh51622ab673a8b6ap10e0b6jsn0f42258aea05";
-const BASE_URL = "https://coinranking1.p.rapidapi.com";
-
-const RAPID_API_HOST = "coinranking1.p.rapidapi.com";
-
-export{ API_KEY, BASE_URL, RAPID_API_HOST};
